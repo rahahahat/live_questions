42daadf805d8a807e2d9a3a039739bc48e87737e
Rahat <rahat4uni@gmail.com>
2020-08-17 20:52:29 +0100

    testing

--- Client/src/components/Nav.js
+++ Client/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const Nav = () => {
+	console.log('HI');
 	return (
 		<div className="center-wrapper">
 			<Link to="/create-room">
