8f8777cda1af7c8ff11ff66d53924ffd70fbada5
Rahat <rahat4uni@gmail.com>
2020-08-18 20:59:19 +0100

    changes

--- Client/src/components/List.js
+++ Client/src/components/List.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Question from "./Questions.js";
+import Question from "./Question.js";
 import io from "socket.io-client";
 // const socket = io('http://localhost:3000');
 const List = ({ dataList, handleVote, handleDelete }) => {
