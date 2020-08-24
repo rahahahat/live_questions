1006f58d56562bde018a20e8c2ec07b712c2a35f
Thomas Hepworth <tomhepworth@hotmail.co.uk>
2020-08-24 15:26:36 +0100

    cleanup

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const path = require('path');
-// // -------------------------------------------
-// const app = express();
-// const server = http.createServer(app);
-// const io = require('socket.io')(server);
-// //  ------------------------------------------
-// io.on('connection', (socket) => {
-// 	console.log('user connected');
-// 	socket.on('disconnect', () => {
-// 		console.log('disconnect');
-// 	});
-// 	// --Exclusive client sockets for handling the question object--
-// 	socket.on('add-question', (data) => {
-// 		socket.to(data.roomName).broadcast.emit('add-this-question', data.obj);
-// 	});
-// 	socket.on('queue-vote-up', (data) => {
-// 		socket.to(data.roomName).broadcast.emit('vote-up-onIndex', data.index);
-// 	});
-// 	socket.on('queue-delete', (data) => {
-// 		socket.to(data.roomName).broadcast.emit('delete-question-onIndex', data.index);
-// 	});
-// 	socket.on('join-room', (roomName) => {
-// 		socket.join(roomName);
-// 	});
-// 	// --------------------------------------------------------------
-// });
-// // -----------s--------------------------------
-// server.listen(3000, () => {
-// 	console.log('The server is running on port 3000');
-// });
-
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
@@ -189,7 +156,6 @@ io.on("connection", (socket) => {
         console.error(err);
       }
     });
-
     socket.to(roomName).emit("delete-question", id);
   });
 
